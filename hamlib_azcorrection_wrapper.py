1980d27988da8e974f46da4e60829ae412272c6f
Lloyd Brown <lloyd_brown@byu.edu>
2016-07-11 11:49:43 -0600

    and more cleanup that I missed

--- hamlib_azcorrection_wrapper.py
+++ hamlib_azcorrection_wrapper.py
@@ -50,12 +50,12 @@ def extractAzEl(inputString = None):
         
 
 if __name__ == "__main__":
-                HOST, PORT = "localhost", 9999
+        HOST, PORT = "localhost", 9999
 
-                # Create the server, binding to localhost on port 9999
-                server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+        # Create the server, binding to localhost on port 9999
+        server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
-                # Activate the server; this will keep running until you
-                # interrupt the program with Ctrl-C
-                server.serve_forever()
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
 
